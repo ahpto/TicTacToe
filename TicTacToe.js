a9344723eb3134cee70919107ab014460a7fde45
ap <aaron.pettman@gmail.com>
2021-02-15 18:12:45 -0500

    updates

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,12 +1,21 @@
-const gameBoard = (function() {  //assigns the module to a variable to be used to call module's methods
-    function _gamePlay() {
+const gameBoard = (() => {  //assigns the module to a variable to be used to call module's methods
+    
+    const player1 = document.querySelector('#player1');
+    const player2 = document.querySelector('#player2');
+    let whoseTurn = 0;
 
+    const whoPlaysFirst = () => {
+        let x = (Math.floor(Math.random() * 2));
+        if (x==0) {player2.textContent = "Player 2 make your move"; whoseTurn = 2;}  
+        else {player1.textContent = "Player 1 make your move"; whoseTurn = 1;}     
     }
+    return {whoPlaysFirst}
+    
 })();
 
 
 
-
+//displays rotating character choice:
 const charChooser = (btnPrev, btnNext, charItem) => {
     
     charItem[0].style.visibility = "visible";
@@ -42,3 +51,28 @@ const charChooser = (btnPrev, btnNext, charItem) => {
 
 charChooser(document.querySelector("#btnPrev"), document.querySelector("#btnNext"), document.querySelectorAll(".charItem1")); 
 charChooser(document.querySelector("#btnPrev2"), document.querySelector("#btnNext2"), document.querySelectorAll(".charItem2"));
+
+
+const player1 = document.querySelector('#player1');
+const player2 = document.querySelector('#player2');
+let whoseTurn = 0;
+
+const whoPlaysFirst = () => {
+    let x = (Math.floor(Math.random() * 2));
+    if (x==0) {player1.textContent = ""; player2.textContent = "Player 2 make your move"; whoseTurn = 2;}  
+    else {player2.textContent = ""; player1.textContent = "Player 1 make your move"; whoseTurn = 1;}     
+}
+
+const newGame = () => {
+    whoPlaysFirst();
+    const gameBoardDivs = document.querySelectorAll(".board");
+    for (i=0; i<gameBoardDivs.length; i++) { gameBoardDivs[i].textContent= "";
+    gameBoardDivs[i].addEventListener("click", (e) => {
+        if (whoseTurn == 2) {if (e.target.textContent == "") {e.target.textContent = "O"; whoseTurn = 1; player2.textContent = ""; player1.textContent = "Player 1 make your move"};} 
+        else {if (e.target.textContent == ""){e.target.textContent = "X"; whoseTurn = 2; player1.textContent = ""; player2.textContent = "Player 2 make your move"};};
+    });
+    }
+
+}
+
+document.querySelector("button").addEventListener("click", newGame);
