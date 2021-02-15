023fc0c6a4a121e3d9487c8dc140e94b1212e2e7
ap <aaron.pettman@gmail.com>
2021-02-14 23:29:08 -0500

    update

---
+++ TicTacToe.js
@@ -0,0 +1,15 @@
+const gameBoard = (function() {  //assigns the module to a variable to be used to call module's methods
+    function _gamePlay() {
+
+    }
+})();
+
+const player = () {
+    function _name = {
+
+    }
+
+}  
+
+const player1 = player();
+const player2 = player(); 
