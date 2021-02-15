3d04887629ae8388f153030d92f7690f7c127e69
ap <aaron.pettman@gmail.com>
2021-02-15 16:24:10 -0500

    updates

--- TicTacToe.js
+++ TicTacToe.js
@@ -5,29 +5,18 @@ const gameBoard = (function() {  //assigns the module to a variable to be used t
 })();
 
 
-const charScroll1 = document.querySelector(".char")
-const btnPrev1 = document.querySelector("#btnPrev")
-const btnNext1 = document.querySelector("#btnNext")
-const charScroll2 = document.querySelector("#char2")
-const btnPrev2 = document.querySelector("#btnPrev2")
-const btnNext2 = document.querySelector("#btnNext2")
-const itemWidth = document.querySelector('.charItem').clientWidth;
-
-btnNext1.addEventListener("click", function(){
-    charScroll2.scrollBy({left: itemWidth, top: 0, behavior: 'smooth'});
-})
 
 
-const charChooser1 = () => {
+const charChooser = (btnPrev, btnNext, charItem) => {
     
-    const charItem = document.querySelectorAll(".charItem1");
     charItem[0].style.visibility = "visible";
     charItem[0].dataKey = "active";
     let nextChar;
+    
 
-    const charDisplay = () => {  
+    const charDisplayPrev = () => {  
         for (i=0; i<charItem.length; i++) {
-            if (i == 2) {nextChar = charItem[0]} else {
+            if (i == 3) {nextChar = charItem[0]} else {
                 nextChar = charItem[i+1]};
          if (charItem[i].dataKey==="active"){charItem[i].style.visibility="hidden"; 
         charItem[i].dataKey = ""; 
@@ -35,7 +24,21 @@ const charChooser1 = () => {
         nextChar.dataKey = "active"; break;}
         } 
     }
-    btnPrev1.addEventListener("click", charDisplay);
+
+    const charDisplayNext = () => {  
+        for (i=0; i<charItem.length; i++) {
+            if (i == 0) {nextChar = charItem[3]} else {
+                nextChar = charItem[i-1]};
+         if (charItem[i].dataKey==="active"){charItem[i].style.visibility="hidden"; 
+        charItem[i].dataKey = ""; 
+        nextChar.style.visibility="visible";
+        nextChar.dataKey = "active"; break;}
+        } 
+    }
+
+    btnPrev.addEventListener("click", charDisplayPrev);
+    btnNext.addEventListener("click", charDisplayNext);
 }
 
-charChooser1(); 
+charChooser(document.querySelector("#btnPrev"), document.querySelector("#btnNext"), document.querySelectorAll(".charItem1")); 
+charChooser(document.querySelector("#btnPrev2"), document.querySelector("#btnNext2"), document.querySelectorAll(".charItem2"));
